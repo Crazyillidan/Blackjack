0ba86175310846b5f2f7c5ef0003fa8ea2ea1d39
Crazyillidan <lucas@fillier.ca>
2024-11-25 19:45:53 -0330

    Third commit

---
+++ db.py
@@ -0,0 +1,12 @@
+import os
+
+def read_chips():
+    if os.path.exists("chips.txt"):
+        with open("chips.txt", "r") as file:
+            return float(file.read())
+    else:   
+        return 100
+
+def write_chips():
+    with open("chips.txt", "w") as file:
+        file.write(str(chips))
