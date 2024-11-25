b8794d490c4e17838bd88370068583900119d2bd
Crazyillidan <lucas@fillier.ca>
2024-11-25 19:25:09 -0330

    First commit

---
+++ Blackjack.py
@@ -0,0 +1,32 @@
+
+
+def title():
+    pass
+        
+    
+def create_deck():
+    pass
+
+
+def calculate_hand_value(hand):
+    pass
+
+
+def display_hand():
+    pass
+
+
+def buy_chips():
+    pass
+
+
+def play_game():
+    pass
+
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
